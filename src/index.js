49e26f4b4c09c9c0559b54b8f37d813276c7fac7
Glav-Tz <GlavTz@gmail.com>
2022-02-19 09:34:43 +0300

    finish tasck

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    function revolution(n) {
+        let strN = String(n);   
+        let newArr = strN.split('')
+        let strRev =newArr.reverse().join('')
+            return strRev
+      }
+      
+    if (n > 0) {    
+      return revolution(n)
+      }else {
+      n = n * -1
+      return revolution(n)
+      } 
 }
